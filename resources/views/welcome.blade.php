1ff13e5abec124bae2eba7021cc5003de0d8d680
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-11-13 21:25:48 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <img src="{{ asset('images/pblogo.png') }}" alt="PB Logo" style="width: 200px; height: auto;">    
+                            <img src="{{ asset('images/pblogo.png') }}" alt="PBLogo" style="width: 200px; height: auto;">    
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
