29487ba5a1fe772928c3fb6af4603765d4d76a41
_FarisZulkiffli <157344137+Kucheing@users.noreply.github.com>
2024-11-14 11:59:24 +0800

    Update welcome.blade.php to correct way

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <img src="{{ asset('images/pblogo.png') }}" alt="PB Logo" style="width: 200px; height: auto;">    
+                            <img src="pblogo.png" alt="logo">    
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
